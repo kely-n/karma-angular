5865598ef1cdfbcf6fed34841da2bc2f71837c67
kely-n <ukirukely@gmail.com>
2021-01-18 14:40:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# karma-angular-ztrstu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/karma-angular-ztrstu)
---
+++ test-files.ts
@@ -0,0 +1,3 @@
+// Import spec (test) files individually
+import "./app/test.ts";
+import "./app/test/calculator.test.js";
